4949c96484657391eff8667bff45a0bb9ccce817
Adam Kabak <adamkabak@yahoo.com>
2022-11-23 13:01:43 -0500

    commit tree 9e4bc5a2db5c33b223b0670f1bf122eff5fae794

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    # validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validate :custom_title_validations
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    def custom_title_validations
+        # unless title.include?("Won't Believe")
+        unless title
+            errors.add(:title, "can't be blank")
+        end
+
+        unless title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "Not click-baity enough")
+        end
+    end
 end
